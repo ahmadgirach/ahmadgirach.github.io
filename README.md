2989bf2d0ec3030e14fe1ee79d99f5d5507770ef
Ahmad Girach <ahmedkhatri6@gmail.com>
2021-10-10 21:35:26 +0530

    [ADD] complete code and github actions

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# ahmadgirach.github.io
-My Personal Portfolio... 
+# Features
+
+- Responsive layout, works well on both Mobile and Desktop
+- Dark mode support. Click buttons to toggle dark mode and light mode.
+- Clear React components and tailwind styling.
+- Exact sections that a developer needs to showcase their skills.
+- Clear call to actions.
+
+# Usage
+
+- Install all the modules by using `npm i` or `npm install` or `yarn`
+- Run the local development server by using `npm run dev` or `yarn dev`
+
+# Credits
+
+Template by
+<a href="https://github.com/manuarora700/simple-developer-portfolio-website" target="_blank" rel="noopener norefferer">Manu Arora</a>
---
+++ components/AboutMe.js
@@ -0,0 +1,195 @@
+import React from "react";
+import userData from "@constants/data";
+
+export default function AboutMe() {
+  return (
+    <section className="bg-white dark:bg-gray-800">
+      <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
+        <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
+          About Me.
+        </h1>
+      </div>
+      <div className="bg-[#F1F1F1] -mt-10 dark:bg-gray-900">
+        <div className="text-container max-w-6xl mx-auto pt-20">
+          <p
+            className="leading-loose text-2xl md:text-4xl font-semibold  mx-4"
+            style={{ lineHeight: "3rem" }}
+          >
+            {userData.about.title}. Currently working on{" "}
+            <a
+              className="bg-red-500 rounded-md px-2 py-1 text-white"
+              href={userData.about.currentProjectUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {userData.about.currentProject} ✈️
+            </a>
+          </p>
+        </div>
+      </div>
+      <div className="bg-[#F1F1F1] dark:bg-gray-900 px-4">
+        <div className="pt-20 grid grid-cols-1 md:grid-cols-3 max-w-6xl mx-auto gap-y-20 gap-x-20">
+          {/* Social Buttons */}
+          <div className="inline-flex flex-col">
+            <div>
+              <h1 className="text-xl font-semibold text-gray-700 dark:text-gray-200">
+                Contact
+              </h1>
+              <p className="text-lg text-gray-500 mt-4 dark:text-gray-300">
+                For any sort help / enquiry, shoot a{" "}
+                <a
+                  href={`mailto:${userData.email}`}
+                  className="text-gray-800 border-b-2 border-gray-800 dark:border-gray-300 font-bold dark:text-gray-300"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  mail
+                </a>{" "}
+                and I'll get back.
+              </p>
+            </div>
+            {/* Social Links */}
+            <h1 className="text-xl font-semibold text-gray-700 mt-8 dark:text-gray-200">
+              Social Links
+            </h1>
+            <div className="mt-4 ml-4">
+              <div className="flex flex-row justify-start items-center ">
+                <a
+                  href={userData.socialLinks.facebook}
+                  className="flex flex-row items-center space-x-4 group"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="my-4">&rarr;</div>
+                  <p className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
+                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
+                    Facebook
+                  </p>
+                </a>
+              </div>
+              <div className="flex flex-row justify-start items-center">
+                <a
+                  href={userData.socialLinks.twitter}
+                  className="flex flex-row items-center space-x-4 group"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="my-4">&rarr;</div>
+                  <p className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
+                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
+                    Twitter
+                  </p>
+                </a>
+              </div>
+              <div className="flex flex-row justify-start items-center">
+                <a
+                  href={userData.socialLinks.github}
+                  className="flex flex-row items-center space-x-4 group"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="my-4">&rarr;</div>
+                  <p className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
+                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
+                    GitHub
+                  </p>
+                </a>
+              </div>
+              <div className="flex flex-row justify-start items-center">
+                <a
+                  href={userData.socialLinks.linkedin}
+                  className="flex flex-row items-center space-x-4 group"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="my-4">&rarr;</div>
+                  <p className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
+                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
+                    LinkedIn
+                  </p>
+                </a>
+              </div>
+              {/* <div className="flex flex-row justify-start items-center">
+                <a
+                  href={userData.socialLinks.twitter}
+                  className="flex flex-row items-center space-x-4 group"
+                >
+                  <div className="my-4">&rarr;</div>
+                  <p className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
+                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-28 group-hover:translate-x-0 transition duration-300"></div>
+                    Instagram
+                  </p>
+                </a>
+              </div> */}
+            </div>
+          </div>
+          {/* Text area */}
+          <div className="col-span-1 md:col-span-2">
+            {userData.about.description?.map((desc, idx) => (
+              <p
+                key={idx}
+                className="text-xl text-gray-700 mb-4 dark:text-gray-300 "
+              >
+                {desc}
+              </p>
+            ))}
+
+            <h1 className="bg-red-500 text-3xl rounded-md px-2 py-1 inline-block font-bold text-gray-50">
+              Tech Stack
+            </h1>
+            <div className="flex flex-row flex-wrap mt-8">
+              <img
+                src="./tech-stack/python.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="./tech-stack/javascript.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="./tech-stack/typescript.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="./tech-stack/django.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="./tech-stack/angular.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="./tech-stack/react.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              {/* <img
+                src="./tech-stack/git.png"
+                className="h-20 w-20 mx-4 my-4"
+              /> */}
+              <img
+                src="./tech-stack/mongodb.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="./tech-stack/html.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="./tech-stack/css.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="./tech-stack/bootstrap.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="./tech-stack/tailwind.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
